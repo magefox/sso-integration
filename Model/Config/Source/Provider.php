1608e70d7c53bc7f8f348b963565cd7b4e3b9ab5
Kend <vanthinh87@gmail.com>
2018-03-10 20:51:35 +0700

    Update system configuration

---
+++ Model/Config/Source/Provider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Magefox\SSOIntegration\Model\Config\Source;
+
+class Provider implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * Retrieve possible customer address types
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [
+            'auth0' => __('Auth0')
+        ];
+    }
+}
