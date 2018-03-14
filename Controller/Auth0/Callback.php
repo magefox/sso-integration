e2438b54ad4720de24102b65f47fc48356dc0c84
kend <vanthinh87@gmail.com>
2018-03-14 21:42:34 +0700

    Update auth0.js

--- Controller/Auth0/Callback.php
+++ Controller/Auth0/Callback.php
@@ -1,10 +1,39 @@
 <?php
 namespace Magefox\SSOIntegration\Controller\Auth0;
 
+use Auth0\SDK\Auth0;
+use Magento\Framework\App\Action\Context;
+use Magefox\SSOIntegration\Model\Auth0Factory;
+
 class Callback extends \Magento\Framework\App\Action\Action
 {
+    /**
+     * @var \Magefox\SSOIntegration\Model\Auth0
+     */
+    protected $auth0;
+
+    public function __construct(
+        Context $context,
+        Auth0Factory $auth0Factory
+    ) {
+        $this->auth0 = $auth0Factory->create();
+        parent::__construct($context);
+    }
+
     public function execute()
     {
-        var_dump($this->getRequest()->getParams());die();
+        $auth0 = new Auth0([
+            'domain'                => $this->auth0->getDomain(),
+            'client_id'             => $this->auth0->getClientId(),
+            'client_secret'         => $this->auth0->getClientSecret(),
+            'redirect_uri'          => $this->auth0->getCallbackUrl(),
+            'audience'              => 'https://magefox.auth0.com/userinfo',
+            'scope'                 => 'openid profile',
+            'persist_id_token'      => true,
+            'persist_access_token'  => true,
+            'persist_refresh_token' => true,
+        ]);
+
+        var_dump($userInfo = $auth0->getUser());die();
     }
 }
